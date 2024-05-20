011995785ef545aabaa18a08bc156f5a33bf3657
soviet <45714679+isoviet@users.noreply.github.com>
2024-05-20 06:41:34 +0300

    Deploy site

--- _symbols/89f7d02d-64f8-4103-8941-40b042e56d19.js
+++ _symbols/89f7d02d-64f8-4103-8941-40b042e56d19.js
@@ -2820,7 +2820,7 @@ function get_each_context_1(ctx, list, i) {
 	return child_ctx;
 }
 
-// (87:6) {#each footer_nav as { link }}
+// (88:6) {#each footer_nav as { link }}
 function create_each_block_1(ctx) {
 	let a;
 	let t_value = /*link*/ ctx[3].label + "";
@@ -2842,7 +2842,7 @@ function create_each_block_1(ctx) {
 		},
 		h() {
 			attr(a, "href", a_href_value = /*link*/ ctx[3].url);
-			attr(a, "class", "svelte-1hqhpqc");
+			attr(a, "class", "svelte-15gsvvc");
 		},
 		m(target, anchor) {
 			insert_hydration(target, a, anchor);
@@ -2861,7 +2861,7 @@ function create_each_block_1(ctx) {
 	};
 }
 
-// (92:6) {#each social as { link, icon }}
+// (93:6) {#each social as { link, icon }}
 function create_each_block(ctx) {
 	let li;
 	let a;
@@ -2900,7 +2900,7 @@ function create_each_block(ctx) {
 		h() {
 			attr(a, "href", a_href_value = /*link*/ ctx[3].url);
 			attr(a, "aria-label", a_aria_label_value = /*icon*/ ctx[4]);
-			attr(a, "class", "svelte-1hqhpqc");
+			attr(a, "class", "svelte-15gsvvc");
 		},
 		m(target, anchor) {
 			insert_hydration(target, li, anchor);
@@ -3020,11 +3020,11 @@ function create_fragment(ctx) {
 			this.h();
 		},
 		h() {
-			attr(span, "class", "copyright svelte-1hqhpqc");
-			attr(nav, "class", "svelte-1hqhpqc");
-			attr(ul, "class", "svelte-1hqhpqc");
-			attr(div, "class", "section-container svelte-1hqhpqc");
-			attr(footer, "class", "svelte-1hqhpqc");
+			attr(span, "class", "copyright svelte-15gsvvc");
+			attr(nav, "class", "svelte-15gsvvc");
+			attr(ul, "class", "svelte-15gsvvc");
+			attr(div, "class", "section-container svelte-15gsvvc");
+			attr(footer, "class", "svelte-15gsvvc");
 		},
 		m(target, anchor) {
 			insert_hydration(target, footer, anchor);
